25db459b5af2846e69319f09bee7a85a0f7e64d9
Nils Nahooy <nils.nahooy@live.com>
2023-10-11 17:54:16 +0200

    Add button functionality

--- ViewModels/NewItemViewModel.cs
+++ ViewModels/NewItemViewModel.cs
@@ -1,7 +1,28 @@
 using System;
+using System.Reactive;
+using ReactiveUI;
+using ToDoApp.DataModel;
 
 namespace ToDoApp.ViewModels {
     public class NewItemViewModel: ViewModelBase {
-        public string Description { get; set; } = String.Empty;
+
+        private string _description = String.Empty;
+
+        public ReactiveCommand<Unit, ToDoItem> OkCommand { get; }
+        public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+        public string Description {
+            get => _description;
+            set => this.RaiseAndSetIfChanged(ref _description, value);
+            }
+
+        public NewItemViewModel() {
+            var isValidObservable = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrWhiteSpace(x) 
+            );
+
+            OkCommand = ReactiveCommand.Create(() => new ToDoItem {Description = Description}, isValidObservable);
+            CancelCommand = ReactiveCommand.Create(() => {});
+        }
     }
 }
